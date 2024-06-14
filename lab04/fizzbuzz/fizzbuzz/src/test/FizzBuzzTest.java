06035aad5778f3646348cae3d4fdefb51b905b04
GaelDupuy <dupuyguillerm1@gmail.com>
2024-06-13 21:26:26 -0400

    Commented Test
    Commented Tests

--- lab04/fizzbuzz/fizzbuzz/src/test/FizzBuzzTest.java
+++ lab04/fizzbuzz/fizzbuzz/src/test/FizzBuzzTest.java
@@ -6,11 +6,11 @@ public class FizzBuzzTest {
 
     @Test
     public void testFizzBuzz() {
-        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
-        assertEquals("Fizz", fizzBuzz.fizzBuzz(3));
-        assertEquals("Buzz", fizzBuzz.fizzBuzz(5));
-        assertEquals("4", fizzBuzz.fizzBuzz(4));
-        assertEquals("7", fizzBuzz.fizzBuzz(7));
-        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(0)); // this test failed 
+        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));   // to test a multiple of both 5 and 3 
+        assertEquals("Fizz", fizzBuzz.fizzBuzz(3));          // to test a multiple of 3 
+        assertEquals("Buzz", fizzBuzz.fizzBuzz(5));             // to test a multiple of 5 
+        assertEquals("4", fizzBuzz.fizzBuzz(4));             // to test a number not a multiple of 3 5 
+        assertEquals("7", fizzBuzz.fizzBuzz(7));            // to test a number not a multiple of 3 5 
+        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(0)); // this test failed first time, works now 
     }
 }
